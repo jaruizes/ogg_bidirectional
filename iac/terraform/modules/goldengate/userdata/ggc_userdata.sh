584aa4f239e7e203520a09208e2c726d439d4f2d
jaruizes <jalb80@gmail.com>
2021-06-21 09:57:58 +0200

    Initial version

---
+++ iac/terraform/modules/goldengate/userdata/ggc_userdata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum install -y unzip
+sudo yum install -y java-1.8.0-openjdk-devel
+sudo yum install -y libnsl
+sudo yum install -y libaio
+sudo yum install -y firewalld
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+
+sudo dd if=/dev/zero of=/root/swapfile bs=1M count=1024
+sudo chmod 600 /root/swapfile
+sudo mkswap /root/swapfile
+sudo swapon /root/swapfile
